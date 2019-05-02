95c7e35ca31e3a34d7a25e38f2f3d5eb5270ea23
nanotubing <nanotubing@users.noreply.github.com>
2019-05-02 15:43:24 -0400

    working on spatial join

--- final_proj.py
+++ final_proj.py
@@ -40,12 +40,23 @@ def spatial_analysis():
 #    print(buffer_file)
 #    print(buffer_suffix)
     arcpy.Buffer_analysis(buffer_file, buffer_suffix, buffer_dist)
-    
-    arcpy.SpatialJoin_analysis(buffer_suffix, papi_store,"in_memory/points_SpatialJoin",\
-                               "JOIN_ONE_TO_MANY", "KEEP_ALL", "", "INTERSECT")
+    stores_per_school = "Healthy_stores_per_school.shp"
+    arcpy.SpatialJoin_analysis(buffer_suffix, papi_store, stores_per_school,\
+                               "JOIN_ONE_TO_ONE", "KEEP_ALL", "", "COMPLETELY_CONTAINS")
 
 #def make_map():
         
 
 [fetch_data(file) for file in zips_to_dl]
 spatial_analysis()
+
+# Replace a layer/table view name with a path to a dataset (which can be a layer file) or create the layer/table view within the script
+# The following inputs are layers or table views: "PhillyPlanning_Schools_buff", "PhillyHealth_Healthy_corner_stores"
+#arcpy.SpatialJoin_analysis(target_features="PhillyPlanning_Schools_buff",\
+#                           join_features="PhillyHealth_Healthy_corner_stores", out_feature_class="C:/Users/tuj53509/Documents/final_project_cschrader/test2.shp", join_operation="JOIN_ONE_TO_ONE", join_type="KEEP_ALL", field_mapping='AUN "AUN" true true false 19 Double 0 0 ,First,#,PhillyPlanning_Schools_buff,AUN,-1,-1;SCHOOL_NUM "SCHOOL_NUM" true true false 10 Long 0 10 ,First,#,PhillyPlanning_Schools_buff,SCHOOL_NUM,-1,-1;LOCATION_I "LOCATION_I" true true false 50 Text 0 0 ,First,#,PhillyPlanning_Schools_buff,LOCATION_I,-1,-1;FACIL_NAME "FACIL_NAME" true true false 64 Text 0 0 ,First,#,PhillyPlanning_Schools_buff,FACIL_NAME,-1,-1;FACILNAME_ "FACILNAME_" true true false 50 Text 0 0 ,First,#,PhillyPlanning_Schools_buff,FACILNAME_,-1,-1;FACIL_ADDR "FACIL_ADDR" true true false 56 Text 0 0 ,First,#,PhillyPlanning_Schools_buff,FACIL_ADDR,-1,-1;ZIPCODE "ZIPCODE" true true false 16 Text 0 0 ,First,#,PhillyPlanning_Schools_buff,ZIPCODE,-1,-1;FACIL_TELE "FACIL_TELE" true true false 16 Text 0 0 ,First,#,PhillyPlanning_Schools_buff,FACIL_TELE,-1,-1;ACTIVE "ACTIVE" true true false 1 Text 0 0 ,First,#,PhillyPlanning_Schools_buff,ACTIVE,-1,-1;GRADE_LEVE "GRADE_LEVE" true true false 24 Text 0 0 ,First,#,PhillyPlanning_Schools_buff,GRADE_LEVE,-1,-1;GRADE_ORG "GRADE_ORG" true true false 8 Text 0 0 ,First,#,PhillyPlanning_Schools_buff,GRADE_ORG,-1,-1;ENROLLMENT "ENROLLMENT" true true false 5 Long 0 5 ,First,#,PhillyPlanning_Schools_buff,ENROLLMENT,-1,-1;TYPE "TYPE" true true false 10 Long 0 10 ,First,#,PhillyPlanning_Schools_buff,TYPE,-1,-1;TYPE_SPECI "TYPE_SPECI" true true false 50 Text 0 0 ,First,#,PhillyPlanning_Schools_buff,TYPE_SPECI,-1,-1;BUFF_DIST "BUFF_DIST" true true false 19 Double 0 0 ,First,#,PhillyPlanning_Schools_buff,BUFF_DIST,-1,-1;ORIG_FID "ORIG_FID" true true false 10 Long 0 10 ,First,#,PhillyPlanning_Schools_buff,ORIG_FID,-1,-1;OFFICIAL_S "OFFICIAL_S" true true false 254 Text 0 0 ,First,#,PhillyHealth_Healthy_corner_stores,OFFICIAL_S,-1,-1;STORE_ADDR "STORE_ADDR" true true false 254 Text 0 0 ,First,#,PhillyHealth_Healthy_corner_stores,STORE_ADDR,-1,-1;CDC_STORE_ "CDC_STORE_" true true false 19 Double 0 0 ,First,#,PhillyHealth_Healthy_corner_stores,CDC_STORE_,-1,-1;CATEGORY "CATEGORY" true true false 254 Text 0 0 ,First,#,PhillyHealth_Healthy_corner_stores,CATEGORY,-1,-1;ZIP "ZIP" true true false 50 Text 0 0 ,First,#,PhillyHealth_Healthy_corner_stores,ZIP,-1,-1', match_option="COMPLETELY_CONTAINS", search_radius="", distance_field_name="")
+#                           
+#arcpy.SpatialJoin_analysis(target_features="PhillyPlanning_Schools_buff", \
+#                           join_features="PhillyHealth_Healthy_corner_stores",\
+#                           out_feature_class="C:/Users/tuj53509/Documents/final_project_cschrader/test2.shp",\
+#                           join_operation="JOIN_ONE_TO_ONE", join_type="KEEP_ALL",\
+#match_option="COMPLETELY_CONTAINS", search_radius="", distance_field_name="")
