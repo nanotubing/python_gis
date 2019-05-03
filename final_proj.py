57af4ca846f0a1597d58e05757e23971a03dc279
nanotubing <nanotubing@users.noreply.github.com>
2019-05-02 21:24:26 -0400

    misc

--- final_proj.py
+++ final_proj.py
@@ -11,56 +11,66 @@ import arcpy, os, urllib2, zipfile
 documents_dir = os.path.expanduser("~\Documents")
 newdir = "final_project_cschrader"
 newpath = os.path.join(documents_dir, newdir)
+downloads_dir = os.path.join(newpath, "data")
 zips_to_dl = ["ftp://ftp.pasda.psu.edu/pub/pasda/philacity/data/PhillyHealth_Healthy_corner_stores.zip",\
               "ftp://ftp.pasda.psu.edu/pub/pasda/philacity/data/PhillyPlanning_Schools.zip",\
               "ftp://ftp.pasda.psu.edu/pub/pasda/philacity/data/PhillyPlanning_Neighborhoods.zip"]
 
 if not os.path.exists(newpath):
     os.makedirs(newpath)
-    
+if not os.path.exists(downloads_dir):
+    os.makedirs(downloads_dir)
 
-def fetch_data(url):
+
+def fetch_data(url, dl_dir):
     download_url = urllib2.urlopen(url)
     zip_contents = download_url.read()
     download_url.close()
-    out_file_name = os.path.join(newpath, os.path.basename(url))
+    out_file_name = os.path.join(dl_dir, os.path.basename(url))
     with open(out_file_name, 'wb') as outf:
         outf.write(zip_contents)
         
     with zipfile.ZipFile(out_file_name, 'r') as zipObj:
-        zipObj.extractall(newpath)
+        zipObj.extractall(dl_dir)
 
-def spatial_analysis():
+def spatial_analysis(dl_dir):
     arcpy.env.workspace = newpath
     arcpy.env.overwriteOutput = True
-    corner_store = "PhillyHealth_Healthy_corner_stores.shp"
-    schools_file = "PhillyPlanning_Schools.shp"
-    schools_buff = schools_file[:-4] + '_buff.shp'
+    
+    corner_store = os.path.join(dl_dir, "PhillyHealth_Healthy_corner_stores.shp")
+    schools_file_basename = "PhillyPlanning_Schools.shp"
+    schools_file_full = os.path.join(dl_dir, schools_file_basename)
+    schools_buff = schools_file_basename[:-4] + "_buff.shp"
     buffer_dist = "300 Meters"
 
-    arcpy.Buffer_analysis(schools_file, schools_buff, buffer_dist)
+    arcpy.Buffer_analysis(schools_file_full, schools_buff, buffer_dist)
     stores_per_school = "Healthy_stores_per_school.shp"
     arcpy.SpatialJoin_analysis(schools_buff, corner_store, stores_per_school,\
                                "JOIN_ONE_TO_ONE", "KEEP_ALL", "", "COMPLETELY_CONTAINS")
-    arcpy.JoinField_management(schools_file, "SCHOOL_NUM", stores_per_school, "SCHOOL_NUM", "Join_Count")
+    arcpy.JoinField_management(schools_file_full, "SCHOOL_NUM", stores_per_school, "SCHOOL_NUM", "Join_Count")
 
 
-def make_map():
-    neighborhoods_shp = "PhillyPlanning_Neighborhoods.shp"
+def make_map(dl_dir, rootpath):
+    arcpy.env.workspace = newpath
+    arcpy.env.overwriteOutput = True
+    
+    neighborhoods_shp = os.path.join(dl_dir, "PhillyPlanning_Neighborhoods.shp")
     mxd_path = os.path.join(os.getcwd(), "final.mxd")
-    output_pdf_name = "Healthy_Stores_PerSchool.pdf"
+    output_pdf_name = "Healthy_Stores.pdf"
     print(mxd_path)
+    
     mxd = arcpy.mapping.MapDocument(mxd_path)
     data_frames = arcpy.mapping.ListDataFrames(mxd)
     data_frame = data_frames[0]
     
-    arcpy.mapping.AddLayer(data_frame, neighborhoods_shp, "BOTTOM" )
+    layer0 = arcpy.mapping.Layer(neighborhoods_shp)
+    arcpy.mapping.AddLayer(data_frame, layer0)
     
     #export to PDF
-    arcpy.mapping.ExportToPDF(mxd, "output_pdf_name")
+    arcpy.mapping.ExportToPDF(mxd, os.path.join(rootpath, output_pdf_name))
 
-[fetch_data(file) for file in zips_to_dl]
+[fetch_data(file, downloads_dir) for file in zips_to_dl]
 
-spatial_analysis()
+spatial_analysis(downloads_dir)
 
-make_map()
+make_map(downloads_dir, newpath)
